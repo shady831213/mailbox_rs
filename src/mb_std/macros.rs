9700ffb0b4e418940a2911e0707befe44d637ac8
shady831213 <shady831213@126.com>
2021-04-07 00:00:35 -0700

    rename

--- src/mb_std/macros.rs
+++ src/mb_std/macros.rs
@@ -8,14 +8,14 @@ macro_rules! export_mb_backdoor_dpi {
         export_mb_backdoor_dpi!(@ u64, $spaces);
         #[no_mangle]
         extern "C" fn mb_backdoor_write_string(
-            name: *const std::os::raw::c_char,
+            space_name: *const std::os::raw::c_char,
             addr: u64,
             data: *const std::os::raw::c_char,
         ) {
-            let name = unsafe { std::ffi::CStr::from_ptr(name) }.to_str().unwrap();
+            let space_name = unsafe { std::ffi::CStr::from_ptr(space_name) }.to_str().unwrap();
             let mut space = $spaces
-                .get(name)
-                .expect(format!("space {} does not exist!", name).as_str())
+                .get(space_name)
+                .expect(format!("space {} does not exist!", space_name).as_str())
                 .lock()
                 .unwrap();
             let m_data = unsafe { std::ffi::CStr::from_ptr(data) }.to_bytes_with_nul();
@@ -24,14 +24,14 @@ macro_rules! export_mb_backdoor_dpi {
 
         #[no_mangle]
         extern "C" fn mb_backdoor_read_string(
-            name: *const std::os::raw::c_char,
+            space_name: *const std::os::raw::c_char,
             addr: u64,
             data: *mut *const std::os::raw::c_char,
         ) {
-            let name = unsafe { std::ffi::CStr::from_ptr(name) }.to_str().unwrap();
+            let space_name = unsafe { std::ffi::CStr::from_ptr(space_name) }.to_str().unwrap();
             let space = $spaces
-                .get(name)
-                .expect(format!("space {} does not exist!", name).as_str());
+                .get(space_name)
+                .expect(format!("space {} does not exist!", space_name).as_str());
             let resolver = crate::mailbox_rs::mb_std::MBSMPtrResolver::new(space);
             let s = resolver.read_c_str(addr as *const u8).unwrap();
             let c_str = std::ffi::CString::new(s.as_str()).unwrap();
@@ -45,25 +45,25 @@ macro_rules! export_mb_backdoor_dpi {
     (@ $t:ty, $spaces:ident) => {
         crate::mailbox_rs::mb_std::paste::paste!{
             #[no_mangle]
-            extern "C" fn [<mb_backdoor_write_ $t>](name: *const std::os::raw::c_char, addr: u64, data: $t) {
-                let name = unsafe { std::ffi::CStr::from_ptr(name) }.to_str().unwrap();
+            extern "C" fn [<mb_backdoor_write_ $t>](space_name: *const std::os::raw::c_char, addr: u64, data: $t) {
+                let space_name = unsafe { std::ffi::CStr::from_ptr(space_name) }.to_str().unwrap();
                 let mut space = $spaces
-                    .get(name)
-                    .expect(format!("space {} does not exist!", name).as_str())
+                    .get(space_name)
+                    .expect(format!("space {} does not exist!", space_name).as_str())
                     .lock()
                     .unwrap();
                 space.write_sized(addr as crate::mailbox_rs::mb_channel::MBPtrT, &data);
             }
             #[no_mangle]
             extern "C" fn [<mb_backdoor_read_ $t>](
-                name: *const std::os::raw::c_char,
+                space_name: *const std::os::raw::c_char,
                 addr: u64,
                 data: *mut $t,
             ) {
-                let name = unsafe { std::ffi::CStr::from_ptr(name) }.to_str().unwrap();
+                let space_name = unsafe { std::ffi::CStr::from_ptr(space_name) }.to_str().unwrap();
                 let space = $spaces
-                    .get(name)
-                    .expect(format!("space {} does not exist!", name).as_str())
+                    .get(space_name)
+                    .expect(format!("space {} does not exist!", space_name).as_str())
                     .lock()
                     .unwrap();
                 let m_data = unsafe { std::slice::from_raw_parts_mut(data as *mut u8, std::mem::size_of::<$t>()) };
